8812012096255f4d2b490eece26d8820e0067fd5
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-05 09:47:23 +0100

    First java bean

---
+++ HelloJavaEE/src/fr/initiation/beans/FirstBean.java
@@ -0,0 +1,13 @@
+package fr.initiation.beans;
+
+public class FirstBean {
+	private String name;
+	private boolean state;
+
+	public FirstBean(String name, boolean state) {
+		this.name = name;
+		this.state = state;
+	}
+
+	
+}
