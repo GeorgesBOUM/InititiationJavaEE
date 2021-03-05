872a8922dc651c881f2d358004e1b86f7d2c9a79
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-05 09:48:07 +0100

    Getters and setters for the bean

--- HelloJavaEE/src/fr/initiation/beans/FirstBean.java
+++ HelloJavaEE/src/fr/initiation/beans/FirstBean.java
@@ -9,5 +9,19 @@ public class FirstBean {
 		this.state = state;
 	}
 
-	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
 }
