9ea0d74059921f3d1ff88d6f5952cb38e28295b9
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-04 21:33:16 +0200

    Formulaire: Définition du bean Profil

---
+++ Formulaires/src/fr/formulaires/beans/Profil.java
@@ -0,0 +1,35 @@
+package fr.formulaires.beans;
+
+public class Profil {
+	
+	private String name;
+	private String mail;
+	private String pass;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	public String getPass() {
+		return pass;
+	}
+	
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+	
+}
