4ef0cf56475076a5d27fbdf503bd4c441cfe1eff
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-05 06:43:53 +0200

    création méthode inscrireUtilisateur

--- Formulaires/src/fr/formulaires/forms/ValidationInscription.java
+++ Formulaires/src/fr/formulaires/forms/ValidationInscription.java
@@ -3,6 +3,10 @@ package fr.formulaires.forms;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import fr.formulaires.beans.Utilisateur;
+
 public final class ValidationInscription {
 	
 	private static final String CHAMP_NOM    = "nom";
@@ -19,6 +23,45 @@ public final class ValidationInscription {
 
     public Map<String, String> getErreurs() {
         return erreurs;
+    }
+    
+    public Utilisateur inscrireUtilisateur( HttpServletRequest request ) {
+        String email = getValeurChamp( request, CHAMP_EMAIL );
+        String motDePasse = getValeurChamp( request, CHAMP_PASS );
+        String confirmation = getValeurChamp( request, CHAMP_CONF );
+        String nom = getValeurChamp( request, CHAMP_NOM );
+
+        Utilisateur utilisateur = new Utilisateur();
+
+        try {
+            validationEmail( email );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_EMAIL, e.getMessage() );
+        }
+        utilisateur.setEmail( email );
+
+        try {
+            validationMotsDePasse( motDePasse, confirmation );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_PASS, e.getMessage() );
+            setErreur( CHAMP_CONF, null );
+        }
+        utilisateur.setMotDePasse( motDePasse );
+
+        try {
+            validationNom( nom );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_NOM, e.getMessage() );
+        }
+        utilisateur.setNom( nom );
+
+        if ( erreurs.isEmpty() ) {
+            resultat = "Succès de l'inscription.";
+        } else {
+            resultat = "Échec de l'inscription.";
+        }
+
+        return utilisateur;
     }
     
 }
