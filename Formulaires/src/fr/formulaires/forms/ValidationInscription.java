c7c83614e9f3f38a533a301b4b4ce486b7c9a072
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-05 07:02:08 +0200

    création méthodes de validation(nom, mdp, mail)

--- Formulaires/src/fr/formulaires/forms/ValidationInscription.java
+++ Formulaires/src/fr/formulaires/forms/ValidationInscription.java
@@ -64,6 +64,38 @@ public final class ValidationInscription {
         return utilisateur;
     }
     
+    private void validationEmail( String email ) throws Exception {
+        if ( email != null ) {
+            if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
+                throw new Exception( "Merci de saisir une adresse mail valide." );
+            }
+        } else {
+            throw new Exception( "Merci de saisir une adresse mail." );
+        }
+    }
+
+    private void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception {
+        if ( motDePasse != null && confirmation != null ) {
+            if ( !motDePasse.equals( confirmation ) ) {
+                throw new Exception( "Les mots de passe entrés sont différents, merci de les saisir à nouveau." );
+            } else if ( motDePasse.length() < 3 ) {
+                throw new Exception( "Les mots de passe doivent contenir au moins 3 caractères." );
+            }
+        } else {
+            throw new Exception( "Merci de saisir et confirmer votre mot de passe." );
+        }
+    }
+
+    private void validationNom( String nom ) throws Exception {
+        if ( nom != null && nom.length() < 3 ) {
+            throw new Exception( "Le nom d'utilisateur doit contenir au moins 3 caractères." );
+        }
+    }
+    
+    private void setErreur( String champ, String message ) {
+        erreurs.put( champ, message );
+    }
+    
     private static String getValeurChamp( HttpServletRequest request, String nomChamp ) {
         String valeur = request.getParameter( nomChamp );
         if ( valeur == null || valeur.trim().length() == 0 ) {
