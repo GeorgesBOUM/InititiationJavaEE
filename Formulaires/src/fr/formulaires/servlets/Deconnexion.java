c1ae053f305ad8cbf9c8a10c7c953424a16fd66c
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-09 19:07:25 +0200

    ajout de la servlet de déconnexion

---
+++ Formulaires/src/fr/formulaires/servlets/Deconnexion.java
@@ -0,0 +1,47 @@
+package fr.formulaires.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Deconnexion
+ */
+@WebServlet(name = "deconnexion", urlPatterns = { "/deconnexion" })
+public class Deconnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final String REDIRECTION = "/connexion";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Deconnexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		response.sendRedirect(REDIRECTION);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
