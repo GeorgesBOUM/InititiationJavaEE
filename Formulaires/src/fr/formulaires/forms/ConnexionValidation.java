7f147e00f6ed8084e74b87b6cf6df8de6b775ac4
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-09 05:18:06 +0200

    définition des erreurs de ConnexionValidation

--- Formulaires/src/fr/formulaires/forms/ConnexionValidation.java
+++ Formulaires/src/fr/formulaires/forms/ConnexionValidation.java
@@ -3,6 +3,10 @@ package fr.formulaires.forms;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import fr.formulaires.beans.Utilisateur;
+
 public class ConnexionValidation {
 	
 	private static final String CHAMP_EMAIL  = "email";
@@ -10,4 +14,19 @@ public class ConnexionValidation {
     
     private String              resultat;
     private Map<String, String> erreurs      = new HashMap<String, String>();
+    
+    public Utilisateur validerConnexion() {
+		
+    	Utilisateur utilisateur = new Utilisateur();
+    	
+    	
+    	
+    	return utilisateur;
+	}
+    
+    private void setErreur(String champ, String message) {
+		this.erreurs.put(champ, message);
+	}
+    
+    
 }
